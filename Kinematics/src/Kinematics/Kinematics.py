5fbf146659471a3bba9d1f6feadf7970ae8069ff
Sebastian Mai <bastinat0r@bastinat0r.de>
2016-11-24 15:07:57 +0100

    adding rotate and translate method
    * transformation class can now do something like a.rotate("x", 10).translate([20, 10, 10])

--- Kinematics/src/Kinematics/Kinematics.py
+++ Kinematics/src/Kinematics/Kinematics.py
@@ -1,7 +1,63 @@
 import numpy as np
 
 class Transformation(object):
+    self.matrix # the 4x4 matrix representing the transformation
+
+
     """A simple class wrapping a standart robot transformation"""
-    def __init__(self):
+    def __init__(self, matrix=np.eye(4,4)):
         """Introduce Epmty Transformation"""
+        self.matrix = matrix
         pass
+
+    def rotate(self, axis, angle="0"):
+        """ rotate given translation by given axis and angle """
+        if axis == "x":
+            return self.rotate_x
+        if axis == "y":
+            return self.rotate_y
+        if axis == "z":
+            return self.rotate_z
+        return self
+
+    def transform(self, matrix):
+        self.matrix = np.dot(self.matrix, matrix)
+        return self
+
+    def translate(self, vector3):
+        """ translate matrix by given vector3 """
+        transform = np.eye(4, 4)
+        transform[0][3] = vector3[0]
+        transform[1][3] = vector3[1]
+        transform[2][3] = vector3[2]
+        return self.transform(transform)
+
+
+    def rotate_x(self, angle):
+        """ rotate around the z axis """
+        transform = np.eye(4, 4)
+        transform[1][1] = cos(angle)
+        transform[1][2] = -1 * sin(angle)
+        transform[2][1] = sin(angle)
+        transform[2][2] = cos(angle)
+        return self.transform(transform)
+
+
+    def rotate_y(self, angle):
+        """ rotate around the z axis """
+        transform = np.eye(4, 4)
+        transform[0][0] = cos(angle)
+        transform[0][2] = -1 * sin(angle)
+        transform[2][0] = sin(angle)
+        transform[2][2] = cos(angle)
+        return self.transform(transform)
+
+
+    def rotate_z(self, angle):
+        """ rotate around the z axis """
+        transform = np.eye(4, 4)
+        transform[0][0] = cos(angle)
+        transform[0][1] = -1 * sin(angle)
+        transform[1][0] = sin(angle)
+        transform[1][1] = cos(angle)
+        return self.transform(transform)
