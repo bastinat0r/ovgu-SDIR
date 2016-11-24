538439e4f67c782ed5649ba07fa1bf476ecc7f62
Sebastian Mai <bastinat0r@bastinat0r.de>
2016-11-24 14:38:23 +0100

    Adding empty class for transformation wrapping

--- Kinematics/src/Kinematics/Kinematics.py
+++ Kinematics/src/Kinematics/Kinematics.py
@@ -1,1 +1,7 @@
+import numpy as np
 
+class Transformation(object):
+    """A simple class wrapping a standart robot transformation"""
+    def __init__(self):
+        """Introduce Epmty Transformation"""
+        pass
